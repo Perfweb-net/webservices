0835f7ffe912e326caec1dd13b26e5d979794f81
Pierre Saugues <psaugues@MBP-de-Pierre-2.lan>
2025-01-23 12:07:46 +0100

    first commit

---
+++ quizzy-front/src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  baseUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000/api',
+  // You will need to create a Firebase project and replace the configuration here with yours
+  firebase: {
+    databaseURL: "https://quizzy-32f58-default-rtdb.europe-west1.firebasedatabase.app",
+    apiKey: "AIzaSyAM4WZA7p1g7OBlDTJ6hTMbM0tDke_fac4",
+    authDomain: "webservices-f2b01.firebaseapp.com",
+    projectId: "webservices-f2b01",
+    storageBucket: "webservices-f2b01.firebasestorage.app",
+    messagingSenderId: "523726044371",
+    appId: "1:523726044371:web:a43315f5e1d18b02466826",
+    measurementId: "G-CVVF9L4JSW"
+  },
+  useSocketIo: true
+};
