5e1764eaeef3385a50b53809b4c89dfc1a572333
devSkyLex <contact@valentin-fortin.pro>
2025-04-02 10:06:15 +0200

    Correction de la validation de la description dans QuizController pour rendre le champ requis

--- quizzy-back/src/Controller/QuizController.php
+++ quizzy-back/src/Controller/QuizController.php
@@ -117,7 +117,7 @@ final class QuizController extends AbstractController
             $title = $params['title'];
         }
         
-        if (!isset($params['description']) || empty($params['description'])) {
+        if (!isset($params['description'])) {
             throw new BadRequestHttpException(message: 'Description is required');
         } else {
             $description = $params['description'];
